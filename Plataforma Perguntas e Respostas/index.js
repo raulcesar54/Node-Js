b317eae4b840b5d8ea59523e21409d0830dac0b4
raulcesar54 <raulcesar54@hotmail.com>
2020-09-06 03:41:30 -0300

    buscando dados do formulario, configurando node para receber dados do formulario

--- Plataforma Perguntas e Respostas/index.js
+++ Plataforma Perguntas e Respostas/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const body = require('body-parser');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+
 // setando o ejs como view engine 
 app.set('view engine', 'ejs')
-
+// arquivos estaticos, como CSS, js, e imagems
+app.use(express.static('public'))
+// decodificar dados enviados pelo formulario
+app.use(bodyParser.urlencoded({ extended: false }))
+// converte dados do formulario para json
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     let nome = 'raul cesar'
     let lang = 'javascript'
-
     res.render('index', { nome, lang })
+})
+app.post('/salvar_dados', (req, res) => {
+    const {pergunta, descricao} = req.body
+    res.send(`pergunta: ${pergunta} descrição:${descricao}`)
 })
 app.listen(8001, error => console.log('Servidor rodando '))
